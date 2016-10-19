f8628be7275c018dc9844a3118db90f97d215272
Nicolas Saenko <nicolas.ozz.com@gmail.com>
2016-10-19 19:11:18 +0300

    fix commands generator

--- lib/generators/shopping_cart/cart_commands_generator.rb
+++
@@ -1,7 +0,0 @@
-class ShoppingCart::CartCommandsGenerator < Rails::Generators::Base
-  source_root File.expand_path('../../../app/commands', __FILE__)
-
-  def generate_commands
-    directory 'commands/', 'app/commands/'
-  end
-end
